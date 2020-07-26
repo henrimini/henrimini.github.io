75de47fef1ad2aa6bae7d0e1335c411f6135ed14
henrimini <henri.deguerry@sfr.fr>
2020-07-26 15:27:29 +0200

    add loading screen

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,14 @@
 var queryURL = "https://cors-anywhere.herokuapp.com/https://emailrep.io/"
+let loadingScreen = $('#loadingScreen');
 
+for(var i=0;i<9;i++) {
+    var x = $('#dancingNotes > li')[i];
+    $(x).css('-webkit-animation','music 1s '+i+'00ms ease-in-out both infinite');
+}
 
 $(document).ready(function() {
     $("#emailrepSubmit").click(function(e) {
+        loadingScreen.modal('show');
         var email = $("#yourEmail").val();
         queryURL += email;
         $.ajax({
@@ -15,7 +21,7 @@ $(document).ready(function() {
             }).done(function (response) {
                 $("#emailrepResult").text(JSON.stringify(response, null, 4));
                 $('#emailrepModal').modal('show');
-                console.log('CORS anywhere response', response);
+                loadingScreen.modal('hide');
             }).fail(function (jqXHR, textStatus) {
                 if(jqXHR.status === 429) {
                     $("#emailrepFail").text("Vous avez effectué trop de requêtes, l'api emailrep ne permet que quelques requêtes gratuites par jour.");
@@ -24,7 +30,10 @@ $(document).ready(function() {
                     $("#emailrepFail").text("La requête n'a pas fonctionné");
                 }
                 $('#emailrepModal').modal('show');
+                loadingScreen.modal('hide');
             })
     });
 });
+
+
 
